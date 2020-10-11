871e26b23776686ec05fbbc4ee04dfdb0030acd0
Fabian maulana <54355288+Miouns@users.noreply.github.com>
2020-10-11 16:26:37 +0700

    Update puppeteer.js

--- puppeteer.js
+++ puppeteer.js
@@ -1,3 +1,9 @@
+const express = require('express');
+const app = express();
+const matter = require('gray-matter');
+const md = require('markdown-it');
+const ejs = require('ejs');
+
 const puppeteer = require('puppeteer');
 
 (async () => {
@@ -8,3 +14,9 @@ const puppeteer = require('puppeteer');
 
   await browser.close();
 })();
+
+app.get('/', (req, res) => {
+  res.send('a');
+})
+
+app.listen(process.env.PORT)
